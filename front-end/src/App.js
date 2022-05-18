2457ef2727e5bf7418c4ab496e842651b2a900b7
thiagoalmeida22 <thiagoguimaraesalmeida@gmail.com>
2022-05-18 17:54:30 -0300

    FIX: corrige o .gitignore

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - Att git ignore!
         </a>
       </header>
     </div>
